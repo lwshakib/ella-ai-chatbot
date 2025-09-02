de44531f702182d03f49da85038cdcf8c5254e9b
lwshakib <leadwithshakib@gmail.com>
2025-09-02 11:14:11 +0600

    Refactor generateAIResponse function to improve error handling and streamline web and image response generation
    - Consolidated previous message retrieval and error handling into a try-catch block.
    - Enhanced web response generation with improved prompt modification and database updates.
    - Added handling for image response generation, including cleaning and saving image URLs.
    - Updated database status handling for failed responses to ensure better error tracking.

--- inngest/functions.ts
+++ inngest/functions.ts
@@ -1,178 +1,204 @@
 import { WEB_SEARCH_SYSTEM_PROMPT } from "@/constants/prompt";
 import { api } from "@/convex/_generated/api";
-import { GoogleGenAI } from "@google/genai";
-import { tavily } from "@tavily/core";
 import { ConvexHttpClient } from "convex/browser";
-import OpenAI from "openai";
+import { ai, imageClient, webClient } from "../config";
 import { EllaSimpleAgent, imagePropertiesIdentifierAgent } from "./agent";
 import { inngest } from "./client";
-import { ai, imageClient, webClient } from "../config"; 
 
 export const generateAIResponse = inngest.createFunction(
   { id: "generate-ai-response" },
   { event: "chat/generate-ai-response" },
   async ({ event, step }) => {
-    const { message, conversationId, clerkId, tool, AIMessageId } =
-      event.data;
+    const { message, conversationId, clerkId, tool, AIMessageId } = event.data;
 
     const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
-
-    const previousMessages = await step.run(
-      "get-previous-messages",
-      async () => {
-        const messages = await convex.mutation(
-          api.functions.getPreviousMessages,
-          {
-            conversationId,
-            clerkId,
-          }
-        );
-        return messages;
-      }
-    );
-
-    if (tool === "web") {
-      const webSearchResult = await step.run(
-        "generate-web-response",
+    try {
+      const previousMessages = await step.run(
+        "get-previous-messages",
         async () => {
-          const webResponse = await webClient.search(message, {
-            includeAnswer: true,
-            includeImages: true,
-            includeImageDescriptions: true,
-            includeFavicon: true,
-            includeRawContent: "text",
-          });
-          const results = webResponse.results;
-          const images = webResponse.images;
-          const resources = results.map((result: any) => ({
-            url: result.url,
-            favicon: result?.favicon as string,
-          }));
-          const answer = webResponse.answer;
-
-          const contents = webResponse.results.map(
-            (result) => result.rawContent
+          const messages = await convex.mutation(
+            api.functions.getPreviousMessages,
+            {
+              conversationId,
+              clerkId,
+            }
           );
-
-          return { results, images, resources, answer, contents };
+          return messages;
         }
       );
 
-      const getModifiedPrompt = await step.run(
-        "get-modified-prompt",
-        async () => {
-          const prompt = WEB_SEARCH_SYSTEM_PROMPT.replace(
-            "{{SEARCH_DETAILS}}",
-            JSON.stringify(webSearchResult.contents)
-          )
-            .replace(
-              "{{SIMPLE_ANSWER}}",
-              webSearchResult.answer?.toString() || ""
-            )
-            .replace("{{PREVIOUS_MESSAGES}}", JSON.stringify(previousMessages));
-
-          return prompt;
-        }
-      );
-
-      const modifyTheWebResponse = await step.run(
-        "modify-web-response",
-        async () => {
-          const response = await ai.models.generateContent({
-            model: "gemini-2.5-flash",
-            contents: message,
-            config: {
-              systemInstruction: getModifiedPrompt,
-            },
-          });
-          return response.text;
-        }
-      );
-
-      const saveTheWebResponseToDB = await step.run("save-to-db", async () => {
-        await convex.mutation(api.functions.updateMessage, {
-          messageId: AIMessageId,
-          text: modifyTheWebResponse as string,
-          type: "web",
-          resources: webSearchResult.resources,
-          images:
-            webSearchResult.images.length > 0
-              ? webSearchResult.images.map((image) => ({
-                  url: image.url,
-                  description: image.description || "",
-                }))
-              : [],
-          status: "completed",
-        });
-      });
-    } else if (tool === "image") {
-      const propertiesWholeResponse =
-        await imagePropertiesIdentifierAgent.run(message);
-
-      const properties = await step.run("clean-the-response", async () => {
-        const propertiesUncleaned = (propertiesWholeResponse.output[0] as any)
-          .content;
-        const propertiesCleaned = propertiesUncleaned
-          .replace(/```json\s*/g, "") // Remove starting ```json
-          .replace(/```/g, "") // Remove ending ```
-          .trim();
-
-        const JsonProperties = JSON.parse(propertiesCleaned);
-        return JsonProperties;
-      });
-
-      if (properties.statusCode === 404) {
-      } else {
-        const generateImageUrl = await step.run(
-          "generate-image-url",
+      if (tool === "web") {
+        const webSearchResult = await step.run(
+          "generate-web-response",
           async () => {
-            const imageResponse = await imageClient.images.generate({
-              model: "black-forest-labs/flux-schnell",
-              response_format: "url",
-              // @ts-ignore
-              response_extension: properties.response_extension,
-              width: properties.width,
-              height: properties.height,
-              num_inference_steps: 4,
-              negative_prompt: properties.negative_prompt,
-              seed: -1,
-              loras: null,
-              prompt: properties.prompt,
+            const webResponse = await webClient.search(message, {
+              includeAnswer: true,
+              includeImages: true,
+              includeImageDescriptions: true,
+              includeFavicon: true,
+              includeRawContent: "text",
             });
-            const generatedImageUrl = (imageResponse as any).data[0].url;
-            return generatedImageUrl;
+            const results = webResponse.results;
+            const images = webResponse.images;
+            const resources = results.map((result: any) => ({
+              url: result.url,
+              favicon: result?.favicon as string,
+            }));
+            const answer = webResponse.answer;
+
+            const contents = webResponse.results.map(
+              (result) => result.rawContent
+            );
+
+            return { results, images, resources, answer, contents };
           }
         );
 
-        const saveImageUrlToDB = await step.run(
-          "save-image-to-db",
+        const getModifiedPrompt = await step.run(
+          "get-modified-prompt",
+          async () => {
+            const prompt = WEB_SEARCH_SYSTEM_PROMPT.replace(
+              "{{SEARCH_DETAILS}}",
+              JSON.stringify(webSearchResult.contents)
+            )
+              .replace(
+                "{{SIMPLE_ANSWER}}",
+                webSearchResult.answer?.toString() || ""
+              )
+              .replace(
+                "{{PREVIOUS_MESSAGES}}",
+                JSON.stringify(previousMessages)
+              );
+
+            return prompt;
+          }
+        );
+
+        const modifyTheWebResponse = await step.run(
+          "modify-web-response",
+          async () => {
+            const response = await ai.models.generateContent({
+              model: "gemini-2.5-flash",
+              contents: message,
+              config: {
+                systemInstruction: getModifiedPrompt,
+              },
+            });
+            return response.text;
+          }
+        );
+
+        const saveTheWebResponseToDB = await step.run(
+          "save-to-db",
           async () => {
             await convex.mutation(api.functions.updateMessage, {
               messageId: AIMessageId,
-              text: "",
-              type: "image",
+              text: modifyTheWebResponse as string,
+              type: "web",
+              resources: webSearchResult.resources,
+              images:
+                webSearchResult.images.length > 0
+                  ? webSearchResult.images.map((image) => ({
+                      url: image.url,
+                      description: image.description || "",
+                    }))
+                  : [],
               status: "completed",
-              imageUrl: generateImageUrl,
+            });
+          }
+        );
+      } else if (tool === "image") {
+        const propertiesWholeResponse =
+          await imagePropertiesIdentifierAgent.run(message);
+
+        const properties = await step.run("clean-the-response", async () => {
+          const propertiesUncleaned = (propertiesWholeResponse.output[0] as any)
+            .content;
+          const propertiesCleaned = propertiesUncleaned
+            .replace(/```json\s*/g, "") // Remove starting ```json
+            .replace(/```/g, "") // Remove ending ```
+            .trim();
+
+          const JsonProperties = JSON.parse(propertiesCleaned);
+          return JsonProperties;
+        });
+
+        if (properties.statusCode === 404) {
+        } else {
+          const generateImageUrl = await step.run(
+            "generate-image-url",
+            async () => {
+              const imageResponse = await imageClient.images.generate({
+                model: "black-forest-labs/flux-schnell",
+                response_format: "url",
+                // @ts-ignore
+                response_extension: properties.response_extension,
+                width: properties.width,
+                height: properties.height,
+                num_inference_steps: 4,
+                negative_prompt: properties.negative_prompt,
+                seed: -1,
+                loras: null,
+                prompt: properties.prompt,
+              });
+              const generatedImageUrl = (imageResponse as any).data[0].url;
+              return generatedImageUrl;
+            }
+          );
+
+          const saveImageUrlToDB = await step.run(
+            "save-image-to-db",
+            async () => {
+              await convex.mutation(api.functions.updateMessage, {
+                messageId: AIMessageId,
+                text: "",
+                type: "image",
+                status: "completed",
+                imageUrl: generateImageUrl,
+              });
+            }
+          );
+        }
+      } else {
+        const AIResponse = await EllaSimpleAgent.run(message);
+        const response = (AIResponse.output[0] as any).content;
+
+        const saveEllaAIResponseToDB = await step.run(
+          "save-ella-ai-response-to-db",
+          async () => {
+            await convex.mutation(api.functions.updateMessage, {
+              messageId: AIMessageId,
+              text: response,
+              type: "text",
+              status: "completed",
             });
           }
         );
       }
-    } else {
-      const AIResponse = await EllaSimpleAgent.run(message);
-      const response = (AIResponse.output[0] as any).content;
+    } catch (error) {
+      // Log the error for debugging
+      console.error("Error in generateAIResponse function:", error);
 
-      const saveEllaAIResponseToDB = await step.run(
-        "save-ella-ai-response-to-db",
-        async () => {
+      // Update the database with failed status
+      await step.run("update-failed-status", async () => {
+        try {
           await convex.mutation(api.functions.updateMessage, {
             messageId: AIMessageId,
-            text: response,
-            type: "text",
-            status: "completed",
+            text: "Something went wrong.",
+            type: tool || "text",
+            status: "failed",
           });
+        } catch (dbError) {
+          console.error(
+            "Failed to update database with error status:",
+            dbError
+          );
         }
-      );
+      });
+
+      // Re-throw the error so Inngest knows the function failed
+      throw error;
     }
   }
 );
